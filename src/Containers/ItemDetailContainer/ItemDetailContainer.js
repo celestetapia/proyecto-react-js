c652ba71fc7beb5cdf15af38ee10a85e77db6ef4
celestetapia <celestetapia66@gmail.com>
2022-12-01 19:09:04 -0300

    Fixeando navegacion hacia item

--- src/Containers/ItemDetailContainer/ItemDetailContainer.js
+++ src/Containers/ItemDetailContainer/ItemDetailContainer.js
@@ -1,8 +1,7 @@
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import { getDoc, collection, doc, getFirestore } from "firebase/firestore";
+import { getDocs, query, where, collection, doc, getFirestore } from "firebase/firestore";
 
 export const ItemDetailContainer = ({ greeting }) => {
   const [product, setProduct] = useState([]);
@@ -10,17 +9,24 @@ export const ItemDetailContainer = ({ greeting }) => {
 
   const { id } = useParams();
   const db = getFirestore();
+  console.log(`Getting information for product: ${id}`);
 
 
   useEffect(() => {
-    const productCollection = collection(db, "productos");
-    const refDoc = doc(productCollection, id);
+    let collectionQuery;
 
-    getDoc(refDoc)
+    const productCollection = collection(db, "items");
+    collectionQuery = query(productCollection, where('__name__', '==', id));
+
+
+    getDocs(collectionQuery)
       .then((result) => {
-        setProduct({id: result.id,
-          ...result.data(),
+        const listProducts = result.docs.map((item) => {
+          return {
+            ...item.data(),
+          };
         });
+        setProduct(listProducts[0]);
       })
       .catch((error) => {
         console.log(error);
